9f73b610858de7a7a92fa52a72d7997573a2c25e
Devendraappa <devendraappa.kune@gmail.com>
2025-01-20 17:06:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "app_instance" {
+resource "aws_instance" "my-ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
@@ -69,14 +69,13 @@ resource "aws_security_group" "app_sg" {
     Name = "ReactAppSG"
   }
 }
-
 output "ec2_instance_public_ip" {
-  value = aws_instance.my_ec2.public_ip
+  value       = aws_instance.my_ec2.public_ip
   description = "Public IP of the EC2 instance"
 }
 
 output "ec2_instance_public_dns" {
-  value = aws_instance.my_ec2.public_dns
+  value       = aws_instance.my_ec2.public_dns
   description = "Public DNS of the EC2 instance"
 }
 
