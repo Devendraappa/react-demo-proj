53948f804bf6c4e6e4f8461a700c8c94ed018264
Devendraappa <devendraappa.kune@gmail.com>
2025-01-20 17:01:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,11 +70,13 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-# Output the public IP of the EC2 instance
 output "ec2_instance_public_ip" {
-  value = aws_instance.app_instance.public_ip
+  value = aws_instance.my_ec2.public_ip
+  description = "Public IP of the EC2 instance"
 }
 
 output "ec2_instance_public_dns" {
-  value = aws_instance.app_instance.public_dns
+  value = aws_instance.my_ec2.public_dns
+  description = "Public DNS of the EC2 instance"
 }
+
