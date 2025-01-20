cc1f921d04acc2b919488e3ad6094e1ac4f8bea7
Devendraappa <devendraappa.kune@gmail.com>
2025-01-20 15:28:15 +0530

    Update remote-backend.tf

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "demobucket-167"
-    key            = "app-react/terraform.tfstate"
+    key            = "appp-dep/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "demotable" 
     encrypt        = true
