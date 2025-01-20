d9486b388a069e609460c639dc6fd8bc4bb29c33
Devendraappa <devendraappa.kune@gmail.com>
2025-01-20 15:17:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,13 +2,11 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "ap-south-1"  # Update this to your desired region
+  default     = "ap-south-1"  # Updated to Mumbai region (ap-south-1)
 }
 
-# S3 Bucket Name
+# S3 Bucket Name (we won't set it dynamically here, instead use random_id in main.tf)
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket where the app will be deployed"
   type        = string
-  default     = "react-app-${random_id.s3_id.hex}"  # This will be used in your main.tf for dynamic naming
 }
-
