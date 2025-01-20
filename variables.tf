463ec48f870420a1b6c50ffd71b418422d5df7b3
Devendraappa <devendraappa.kune@gmail.com>
2025-01-20 18:19:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,37 @@
+# AWS Region
 variable "aws_region" {
-  default = "ap-south-1"
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
 }
 
+# AMI ID for EC2 instance
 variable "ami_id" {
-  default = "ami-00bb6a80f01f03502" # Replace with the correct AMI ID for your region
+  description = "AMI ID for the EC2 instance"
+  type        = string
 }
 
+# EC2 Instance Type
 variable "instance_type" {
-  default = "t2.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
-variable "key_name" {
-  default = "desktop" # Replace with your actual key pair name
+# Subnet ID for the EC2 instance
+variable "subnet_id" {
+  description = "The subnet ID in which the EC2 instance will be created"
+  type        = string
+}
+
+# VPC ID
+variable "vpc_id" {
+  description = "The VPC ID where the security group will be applied"
+  type        = string
+}
+
+# Path to the public key for EC2 instance
+variable "public_key_path" {
+  description = "Path to the SSH public key file for EC2 instance"
+  type        = string
 }
