e691c87b0ee44be38c41627acab391a638eaea5a
Devendraappa <devendraappa.kune@gmail.com>
2025-01-20 15:24:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_s3_bucket" "app_bucket" {
-  bucket = "react-app-${random_id.s3_id.hex}"
-  acl    = "public-read"
+  region = var.aws_region  # The region will now be set to "ap-south-1"
 }
 
 resource "random_id" "s3_id" {
   byte_length = 8
 }
 
-# You can add more resources like CloudFront, S3 object deployment, etc., if needed.
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "react-app-${random_id.s3_id.hex}"  # Dynamically using random_id
+  acl    = "public-read"
+}
+
