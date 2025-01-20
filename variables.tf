614f18c5f731908d2db5824aaaa8513e6c58b771
Devendraappa <devendraappa.kune@gmail.com>
2025-01-20 15:16:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,14 @@
+# AWS Region for resource deployment
 variable "aws_region" {
-  description = "AWS region where resources will be created"
-  default     = "ap-south-1"
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"  # Update this to your desired region
 }
 
+# S3 Bucket Name
 variable "s3_bucket_name" {
-  description = "Base name for the S3 bucket"
-  default     = "my-react-app"  # Base name without the random suffix
+  description = "Name of the S3 bucket where the app will be deployed"
+  type        = string
+  default     = "react-app-${random_id.s3_id.hex}"  # This will be used in your main.tf for dynamic naming
 }
 
-variable "aws_account_id" {
-  description = "AWS Account ID for SNS and other resources"
-  default     = "123456789012"
-}
-
-variable "role_name" {
-  description = "IAM role name to assume in GitHub Actions"
-  default     = "MyTerraformRole"
-}
